9e1eec4299b34676543638f0f951962c717301da
jeanrenejaouen <jean-rene.jaouen@wanadoo.fr>
2023-12-29 15:38:03 +0100

    Gestion flèche droite

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,43 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+/* RECUPERER LES ELEMENTS ET DECLARER LES VARIABLES */
+let text = document.getElementById("text");
+let flecheDroite = document.querySelector(".arrow_right");
+let flecheGauche = document.querySelector(".arrow_left");
+let index = 0;
+let listeDot = document.getElementsByClassName("dot");
+let baliseImage = document.getElementById("banner-img");
+
+/* GESTION DE LA FLECHE DROITE */
+flecheDroite.addEventListener ("click", () => {
+	console.log ("Vous avez cliqué sur la flèche droite");
+	index+= 1;
+	if(index===4)index=0;
+	baliseImage.src = "./assets/images/slideshow/"+slides[index].image;
+	text.innerHTML=slides[index].tagLine;
+	console.log(index);	
+	for (let i=0; i<listeDot.length; i++){
+		console.log (listeDot[i]);
+		listeDot[i].className="dot";				
+	}
+	listeDot[index].className="dot dot_selected";	
+});
+	
+/* GESTION DE LA FLECHE GAUCHE */
+/* flecheGauche.addEventListener ("click", () => {
+	console.log ("Vous avez cliqué sur la flèche gauche")	
+	if(index===0)index=4;
+	index=index-1;
+	console.log(index);
+	baliseImage.src = "./assets/images/slideshow/"+slides[index].image;
+	text.innerHTML=slides[index].tagLine;
+	for (let i=0; i<listeDot.length; i++){
+		console.log (listeDot[i]);
+		listeDot[i].className="dot";				
+	}
+	listeDot[index].className="dot dot_selected";
+	
+}); */
